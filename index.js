5b6dd79df3fd75772f7f8099ffec035c58a0dd62
margaret wambui ndung'u <wambuindungu4967@gmail.com>
2024-07-27 22:47:45 +0400

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Max",
+    streetAddress: "5 Main Street",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Alternate using ES6 Spread operators:
+    // return { ...employee, ...{ [key]: value } }
+    return Object.assign({}, employee, { [key]: value });
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    // Alternate using ES6 Spread operators:
+    // const newObj = { ...employee }
+    const newObj = Object.assign({}, employee);
+  
+    delete newObj[key];
+  
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+    return employee;
+  }
